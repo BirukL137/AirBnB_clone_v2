970532a6a88a6530b9dbfd4ca1c3b8a82a81c176
BirukL137 <buraf13k@gmail.com>
2023-01-13 20:54:27 -0800

    2. C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''A script that starts a Flask web application'''
+''' starts a Flask web application '''
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,13 +7,17 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    '''displays "Hello HBNB!" for route: /'''
+    '''
+    displays "Hello HBNB!" for route: /
+    '''
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hello():
-    '''displays "HBNB" for route: /hbnb'''
+    '''
+    displays "HBNB" for route: /hbnb
+    '''
     return 'HBNB'
 
 
@@ -23,8 +27,9 @@ def c_is_fun(text):
     - displays "C" followed by value of the text variable
     - replaces underscore "_" with a space " "
     '''
-    return 'C' + text.replace('_', ' ')
+    return 'C ' + text.replace('_', ' ')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    ''' main function '''
     app.run(host='0.0.0.0', port=5000)
